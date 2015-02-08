72882fc9105a8d978b26a338bda465e27d228736
Ajunpreet Bambrah <ajun@Ajunpreets-MacBook-Pro.local>
2015-02-08 13:05:18 -0800

    Add Progress Bars

--- js/custom.js
+++ js/custom.js
@@ -4,5 +4,11 @@ jQuery(function($){
 		slides  :  	[ {image : '/images/mainback.jpg', title : 'Main Background'} ]
 	});
 
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+
 });
 		    
