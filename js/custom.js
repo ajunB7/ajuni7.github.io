b547f5be7182b1209288d8ef4df0699df0f4727f
Ajunpreet Bambrah <ajun@Ajunpreets-MacBook-Pro.local>
2015-02-08 16:27:40 -0800

    Working Scrollto

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,11 @@ jQuery(function($){
 		slides  :  	[ {image : '/images/mainback.jpg', title : 'Main Background'} ]
 	});
 
+	$('.navbar-nav a').click(function(){
+	  $.scrollTo( this.hash, 1500, { easing:'swing' });
+	  return false;
+	});
+
 	$(window).scroll(function() {
 		if ($('.skillbar').is(':visible')) {
 			jQuery('.skillbar').each(function(){
